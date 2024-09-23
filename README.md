88dc7d4c506cbde97a4b3641214b3734b24bd934
Rochelle Charles <roccharl@amazon.co.uk>
2024-09-23 18:00:10 +0100

    queue hasSpace, queue isEmpty

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Enqueue - adds data to the “back” or end of the queue
 Dequeue - provides and removes data from the “front” or beginning of the queue
 Peek - reveals data from the “front” of the queue without removing it
 bounded queue = queue with a limit to the amount of data that can be added 
+
+Underflow = trying to remove elements from an empty queue.
--- src/main/java/org/example/Queues/QueueMaxSize.java
+++ src/main/java/org/example/Queues/QueueMaxSize.java
@@ -17,10 +17,49 @@ public class QueueMaxSize {
         this.maxSize = maxSize;
     }
 
+    //method returns true if the queue has
+    //space for another node
+    public boolean hasSpace() {
+        if (this.size < this.maxSize) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //method checks if the queue is empty
+    //returns true if empty
+    public boolean isEmpty() {
+        if (this.size == 0){
+            return true;
+        }
+        return false;
+    }
+
     public void enqueue(String data) {
         this.queue.addToTail(data);
         this.size ++;
         System.out.println("Added " + data + "! Queue size is now " + this.size);
+    }
+
+    public String dequeue() {
+        if (!this.isEmpty()) {
+            String data = this.queue.removeHead();
+            this.size--;
+            System.out.println("Removed " + data + "! Queue size is now " + this.size);
+            return data;
+        } else {
+            throw new Error("Queue is empty!");
+        }
+    }
+
+    public String peek() {
+        if (this.isEmpty()) {
+            return null;
+        } else {
+            return this.queue.head.data;
+        }
+
     }
 
     public static void main(String[] args) {
@@ -35,5 +74,34 @@ public class QueueMaxSize {
         coffeeOrder.enqueue("latte");
         coffeeOrder.enqueue("espresso");
         coffeeOrder.enqueue("cappuccino");
+        coffeeOrder.peek();
+
+        QueueMaxSize smoothieOrders = new QueueMaxSize();
+        smoothieOrders.enqueue("strawberry banana");
+        smoothieOrders.enqueue("chocolate peanut butter");
+        System.out.println("Blending the smoothies...");
+        smoothieOrders.dequeue();
+        smoothieOrders.dequeue();
+        System.out.println("All orders are complete!");
+
+        System.out.println(" ");
+        QueueMaxSize queueOne = new QueueMaxSize(25);
+        QueueMaxSize queueTwo = new QueueMaxSize(0);
+        System.out.println("queueOne has space for more nodes: " + queueOne.hasSpace());
+        System.out.println("queueTwo has space for more nodes: " + queueTwo.hasSpace());
+        System.out.println("queueOne is empty: " + queueOne.isEmpty());
+        System.out.println("queueTwo is empty: " + queueTwo.isEmpty());
+
+        System.out.println("");
+
+        QueueMaxSize boundedQueue = new QueueMaxSize(3);
+        boundedQueue.enqueue("one");
+        boundedQueue.enqueue("two");
+        boundedQueue.enqueue("three");
+        boundedQueue.dequeue();
+        boundedQueue.dequeue();
+        boundedQueue.dequeue();
+        boundedQueue.dequeue();
+
     }
 }
